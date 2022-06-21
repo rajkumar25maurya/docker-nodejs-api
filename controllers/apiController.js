3668bd37fb5e0a4f77f1061387732bf41c1d6cdd
Raj Maurya <rajkumar25maurya@gmail.com>
2022-06-21 09:51:22 +0530

    add docker api proj

---
+++ controllers/apiController.js
@@ -0,0 +1,34 @@
+const Users = require('../models/userModel');
+const bodyParser = require('body-parser');
+
+module.exports = (app) => {
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/api/users', (req, res) => {
+        Users.find({}, (err, users) => {
+            if (err) {
+                res.status(400).send({
+                    message: err.message
+                });
+            }
+            res.send(users);
+        })
+    })
+
+    app.post('/api/user', (req, res) => {
+        let user = Users({
+            username: req.body.username,
+            address: req.body.address
+        });
+        user.save((err) => {
+            if (err) {
+                res.status(400).send({
+                    message: err.message
+                });
+            }
+            res.send('User created successfully!')
+        })
+
+    });
+}
